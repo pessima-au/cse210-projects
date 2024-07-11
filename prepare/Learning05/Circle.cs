b2f804cf21061f9fcf1267d3bca2127c63facaf2
pessima_au <albertpessima@outlook.com>
2024-07-11 14:03:18 +0000

    Completed Week 5 learning activity

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,17 @@
+using System.Drawing;
+using System.Reflection.Metadata.Ecma335;
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+
+
+    public override double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
 
-class Program
+
+List<Shape> shapes = new List<Shape>();
+
+shapes.Add(new Square("Green", 6));
+shapes.Add(new Rectangle("White",11, 7));
+shapes.Add(new Circle("Blue", 8));
+
+
+foreach (Shape shape in shapes)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning05 World!");
-    }
+    double calculateArea = shape.GetArea();
+    Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {calculateArea}");
 }
+
